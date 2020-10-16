f5c8e1bc368f0e3675c8b3a5b419b02cb4150f07
Azzary <43534730+Azzary@users.noreply.github.com>
2020-10-17 07:48:06 +1100

    big update

--- main.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-class main()
-
-    def __init__(self):
-        self.leaf_mitm = LeafMITM(("34.251.172.139",5555))
--- map_gestion/entity.py
+++
@@ -1,9 +0,0 @@
-class Entity:
-    def __init__(self, type, cell=0, **kwargs):
-        self.type = type
-        self.cell = cell
-        self.isMainCharacter = False
-        self.__dict__.update(kwargs)
-
-    def __str__(self):
-        return str(self.__dict__)
